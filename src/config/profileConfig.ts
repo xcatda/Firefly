27930df54949c39579d91e41b3f2eefee07cc0f7
xcatda <g13217476558@163.com>
2026-07-09 16:48:12 +0800

    修改了个人信息的链接内容

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -12,7 +12,7 @@ export const profileConfig: ProfileConfig = {
 	name: "Leon",
 
 	// 个人签名
-	bio: "Hello, I'm Leon.",
+	bio: "么么哒，>_<",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -24,19 +24,19 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "https://qm.qq.com/q/phje9ftc3K",
 			showName: false,
 		},
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/xcatda",
 			showName: false,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "Leon_Mrc@outlook.com",
 			showName: false,
 		},
 		{
